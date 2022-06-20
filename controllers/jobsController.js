b25656eba67e76918ab6f3e732bd6aa2605e1c77
alonso537 <58403576+alonso537@users.noreply.github.com>
2022-06-19 21:08:33 -0500

    monthly

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -7,6 +7,7 @@ import {
 } from "../errors/index.js";
 import checkPermissions from "../utils/CheckPermissions.js";
 import mongoose from "mongoose";
+import moment from "moment";
 
 const createJob = async (req, res, next) => {
   try {
@@ -161,9 +162,47 @@ const showStats = async (req, res, next) => {
       declined: stats.declined || 0,
     };
 
-    let monthyApplications = [];
+    let monthlyApplications = await Job.aggregate([
+      {
+        $match: {
+          createdBy: mongoose.Types.ObjectId(req.user.userId),
+        },
+      },
+      {
+        $group: {
+          _id: {
+            year: { $year: "$createdAt" },
+            month: { $month: "$createdAt" },
+          },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $sort: {
+          "_id.year": -1,
+          "_id.month": -1,
+        },
+      },
+      {
+        $limit: 6,
+      },
+    ]);
 
-    res.status(StatusCodes.OK).json({ defaultStats, monthyApplications });
+    monthlyApplications = monthlyApplications
+      .map((item) => {
+        const {
+          _id: { year, month },
+          count,
+        } = item;
+        const date = moment()
+          .month(month - 1)
+          .year(year)
+          .format("MMM Y");
+        return { date, count };
+      })
+      .reverse();
+
+    res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
   } catch (error) {
     next(error);
   }
